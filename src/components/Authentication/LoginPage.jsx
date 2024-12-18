a32eb9c9e761cb7a09b698bd2d5914dee48f5d3e
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-12-18 13:36:35 +0800

    started working on login page and basic form

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,56 @@
+import React, { useRef } from 'react';
+
+import './LoginPage.css';
+
+const LoginPage = () => {
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name: '',
+      phone: 0,
+    };
+    user.name = nameRef.current.value;
+    user.phone = phoneRef.current.value;
+    console.log(user);
+  };
+
+  return (
+    <section className='align_center form_page'>
+      <form action='' className='authentication_form' onSubmit={handleSubmit}>
+        <h2>Login Form</h2>
+        <div className='form_inputs'>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              ref={nameRef}
+              id='name'
+              className='form_text_input'
+              placeholder='Enter your name'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='phone'>Phone Number</label>
+            <input
+              type='number'
+              ref={phoneRef}
+              id='phone'
+              className='form_text_input'
+              placeholder='Enter your phone number'
+            />
+          </div>
+
+          <button type='submit' className='search_button form_submit'>
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
