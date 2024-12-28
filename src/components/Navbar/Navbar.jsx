fa19e21861a3cc2151c9af89d0ca09a97a25f61f
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-12-28 09:13:56 +0800

    added react routing and fixex minor css bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,11 @@ import memo from '../../assets/memo.png';
 import order from '../../assets/package.png';
 import lock from '../../assets/locked.png';
 import LinkWithIcon from './LinkWithIcon';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='align_center navbar'>
+    <nav className='align_center navbar'>
       <div className='align_center'>
         <h1 className='navbar_heading'>Sola Tech</h1>
         <form className='align_center navbar_form'>
@@ -25,17 +26,18 @@ const Navbar = () => {
           </button>
         </form>
       </div>
-      <div className='align-center navbar_links'></div>
-      <LinkWithIcon title='Home' link='/' emoji={rocket} />
-      <LinkWithIcon title='Products' link='/products' emoji={star} />
-      <LinkWithIcon title='LogIn' link='/login' emoji={idButton} />
-      <LinkWithIcon title='SignUp' link='/signup' emoji={memo} />
-      <LinkWithIcon title='My Orders' link='/myoders' emoji={order} />
-      <LinkWithIcon title='Logout' link='/logout' emoji={lock} />
-      <a href='/cart' className='align_center'>
-        Cart <p className='align_center cart_counts'>0</p>
-      </a>
-    </div>
+      <div className='align_center navbar_links'>
+        <LinkWithIcon title='Home' link='/' emoji={rocket} />
+        <LinkWithIcon title='Products' link='/products' emoji={star} />
+        <LinkWithIcon title='LogIn' link='/login' emoji={idButton} />
+        <LinkWithIcon title='SignUp' link='/signup' emoji={memo} />
+        <LinkWithIcon title='My Orders' link='/myoders' emoji={order} />
+        <LinkWithIcon title='Logout' link='/logout' emoji={lock} />
+        <NavLink to='/cart' className='align_center'>
+          Cart <p className='align_center cart_counts'>0</p>
+        </NavLink>
+      </div>
+    </nav>
   );
 };
 
